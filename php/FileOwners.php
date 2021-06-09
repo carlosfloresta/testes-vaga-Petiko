74b2256f13698749d2ba87ba3fda4c25ba1e911a
Carlos Floresta <47724025+carlosfloresta@users.noreply.github.com>
2021-06-08 21:01:17 -0300

    atualizado em 08/06/2021

--- php/FileOwners.php
+++ php/FileOwners.php
@@ -1,26 +1,30 @@
 <?php
 
-
 /* ----------------- DESCRIÇÃO DO TESTE -----------------------*/
 
 /*
-Implemente uma função que ao receber um array associativo contendo arquivos e donos, retorne os arquivos agrupados por dono. 
+Implemente uma função que ao receber um array associativo contendo arquivos e donos, retorne os arquivos agrupados por dono.
 
 Por exempolo, um array ["Input.txt" => "Jose", "Code.py" => "Joao", "Output.txt" => "Jose"] a função groupByOwners deveria retornar ["Jose" => ["Input.txt", "Output.txt"], "Joao" => ["Code.py"]].
 
-
-*/
+ */
 
 class FileOwners
 {
     public static function groupByOwners($files)
     {
-        return NULL;
+        $array1 = array_values($files);
+        $array2 = array_keys($files);
+        $array = array();
+        foreach ($array1 as $key => $value) {
+            $array[$value][$key] = $array2[$key];
+        }
+        return $array;
     }
 }
 
 $files = array
-(
+    (
     "Input.txt" => "Jose",
     "Code.py" => "Joao",
     "Output.txt" => "Jose",
@@ -28,4 +32,4 @@ $files = array
     "Out.php" => "maria",
 
 );
-var_dump(FileOwners::groupByOwners($files));
+print_r(FileOwners::groupByOwners($files));
